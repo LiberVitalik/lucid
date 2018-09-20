3d537aec487836fa25cfd32b3f301681168e5cb2
Vitali Liber <libervitalik@gmail.com>
2018-09-20 21:47:45 +0300

    change structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     less = require('gulp-less'),
+    del = require('del'),
+    replace = require('gulp-replace'),
     markdown = require('markdown'),
     fileInclude = require('gulp-file-include');
 
 gulp.task('fileInclude', function() {
-    gulp.src(['app/template/index.html'])
+    gulp.src(['src/template/index.html'])
         .pipe(fileInclude({
             prefix: '@@',
             basepath: '@file',
@@ -13,13 +15,25 @@ gulp.task('fileInclude', function() {
                 markdown: markdown.parse
             }
         }))
-        .pipe(gulp.dest('./app/'));
+        .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('replaceHtml', function(){
+    gulp.src(['src/index.html'])
+        .pipe(replace('../', ''))
+        .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('replaceStyle', function(){
+    gulp.src(['src/css/style.css'])
+        .pipe(replace('../', ''))
+        .pipe(gulp.dest('src/css'));
 });
 
 gulp.task('less', function () {
-    return gulp.src('app/template/*.less')
+    return gulp.src('src/template/*.less')
         .pipe(less())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({
             stream: true
         }));
@@ -28,18 +42,29 @@ gulp.task('less', function () {
 gulp.task('browser-sync', function () {
     browserSync({
         server: {
-            baseDir: 'app'
+            baseDir: 'src'
         },
         notify: false
     })
 });
 
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'less', 'fileInclude', 'replaceHtml', 'replaceStyle'], function() {
+    gulp.src('src/css/*.css').pipe(gulp.dest('dist/css'));
+    gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+    gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'));
+    gulp.src('src/*.html').pipe(gulp.dest('dist'));
+});
+
 gulp.task('watch', ['fileInclude', 'browser-sync', 'less'], function () {
-    gulp.watch('app/template/**/*.less', ['less']);
-    gulp.watch('app/template/*.less', ['less']);
-    gulp.watch('app/less_mixin/*.less', ['less']);
-    gulp.watch('app/template/**/*.html', ['fileInclude'], browserSync.reload);
-    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('src/template/**/*.less', ['less']);
+    gulp.watch('src/template/*.less', ['less']);
+    gulp.watch('src/less_mixin/*.less', ['less']);
+    gulp.watch('src/template/**/*.html', ['fileInclude'], browserSync.reload);
+    gulp.watch('src/*.html', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
